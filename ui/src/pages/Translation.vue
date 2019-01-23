4f551db85c87ebbab3eb06844b08606c73ab68cf
Connor Wagner <connorawagner@icloud.com>
2019-01-23 15:53:46 -0500

    Translation UI

---
+++ ui/src/pages/Translation.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container> <TranslationTable /> </v-container>
+</template>
+
+<script>
+import TranslationTable from "./../components/translation/TranslationTable";
+
+export default {
+  name: "Translation",
+  components: { TranslationTable }
+};
+</script>
+
+<style scoped></style>
